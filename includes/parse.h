d8f298e9570f22fa529e0d8aa0f2c8dab3282363
Marti Vallhonrat Rafart <mvallhon@car13s2.42barcelona.com>
2024-03-20 19:20:02 +0100

    start categorization

--- includes/parse.h
+++ includes/parse.h
@@ -12,12 +12,24 @@ typedef struct s_operator
 	char 	reoutput;
 }	t_operators;
 
+typedef struct s_word
+{
+	char			*com;
+	char			**flags;
+	int				token;
+	struct s_word	*next;
+}	t_word;
 
+//ESTO ES SPLIT
 int		check_if_operator(t_operators *data, char c);
 void	free_mem(char **split);
 void	init_data(t_operators *data);
 int		is_operator(char *line, int index, t_operators *data);
 void	get_word_quote(char **word, char *line, int *index, char c);
 int		count_words(char *line, const char *set, t_operators *data);
+//ESTO ES CATEGORIZE
+void	categorize(char **words, t_word **lst);
+t_word	*new_word(char **words, int start, int end);
+void	wordadd_back(t_word **words, t_word *new_word);
 
 #endif
