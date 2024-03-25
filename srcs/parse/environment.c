7f0b5ade56d9803636839265a49904e92f1f65b5
Marti Vallhonrat Rafart <mvallhon@car13s2.42barcelona.com>
2024-03-25 15:17:57 +0100

    new get_env

--- srcs/parse/environment.c
+++ srcs/parse/environment.c
@@ -1,5 +1,17 @@
 #include "../../includes/parse.h"
 
+char	*get_env(t_list **lst, char *key)
+{
+	t_list	*aux;
+
+	aux = *lst;
+	while (aux && ft_strncmp(aux, key, ft_strlen(key) != 0))
+		aux = aux->next;
+	if (!aux)
+		return (NULL);
+	return (aux->content);
+}
+
 void	parse_environment(t_list **lst, char **envp)
 {
 	t_list	*aux;
