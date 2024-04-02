c7472f4fbdd3071336c2478b0c4467eb02d022c7
Marti Vallhonrat Rafart <mvallhon@car13s2.42barcelona.com>
2024-04-02 19:14:02 +0200

    new env

--- srcs/parse/environment.c
+++ srcs/parse/environment.c
@@ -6,13 +6,14 @@
 /*   By: rcortes- <rcortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:22:55 by rcortes-          #+#    #+#             */
-/*   Updated: 2024/03/26 15:22:56 by rcortes-         ###   ########.fr       */
+/*   Updated: 2024/04/02 17:58:49 by mvallhon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parse.h"
 
-char	*get_env(t_list **lst, char *key)
+//ENVIROMENT ANTIGUO !NO TOCAR!
+/*char	*get_env(t_list **lst, char *key)
 {
 	t_list	*aux;
 	int		i;
@@ -41,5 +42,41 @@ void	parse_environment(t_list **lst, char **envp)
 	{
 		aux = ft_lstnew(envp[i]);
 		ft_lstadd_back(lst, aux);
+	}
+}*/
+
+char	*get_env(t_env **env_lst, char *get_key)
+{
+	t_env	*aux;
+	
+	aux = *env_lst;
+	while (aux)
+	{
+		if (ft_strcmp(get_key, aux->key) == 0)
+			return (aux->value);
+		aux = aux->next;
+	}
+	return (NULL);
+}
+
+void	parse_enviroment(t_env **env_lst, char **envp)
+{
+	t_env	*aux;
+	int		i;
+	int		j;
+
+	*env_lst = NULL;
+	i = -1;
+	while (envp[++i])
+	{
+		j = 0;
+		aux = ft_newenv(envp[i]);
+		while (envp[i][j] != '=')
+			j++;
+		aux->key = ft_substr(envp[i], 0, j);
+		j++;
+		aux->value = ft_substr(envp[i], j, ft_strlen(envp[i]));
+		aux->only_exp = 0;
+		ft_envadd_back(env_lst, aux);
 	}
 }
---
+++ srcs/parse/environment_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environment_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvallhon <mvallhon@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 16:50:42 by mvallhon          #+#    #+#             */
+/*   Updated: 2024/04/02 17:58:51 by mvallhon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/parse.h"
+
+t_env	*ft_newenv(char *env)
+{
+	t_env	*new;
+
+	new = (t_env *)malloc(sizeof(new));
+	if (!new)
+		return (NULL);
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_envadd_back(t_env **env_lst, t_env *new_env)
+{
+	t_env	*aux;
+
+	if (!new_env)
+		return ;
+	if (*env_lst == NULL)
+	{
+		*env_lst = new_env;
+		return ;
+	}
+	aux = *env_lst;
+	while (aux->next != NULL)
+		aux = aux->next;
+	aux->next = new_env;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
