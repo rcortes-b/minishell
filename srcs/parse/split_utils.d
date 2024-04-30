ec5c1ede88e14bd65e52dfcecf9dc19196675b89
Raul Cortes Benitez <rcortes-@car5s6.42barcelona.com>
2024-04-30 13:04:02 +0200

    final PUSHgit add .git add .

--- srcs/parse/split_utils.d
+++
@@ -1,8 +0,0 @@
-srcs/parse/split_utils.o: srcs/parse/split_utils.c \
-  srcs/parse/../../includes/parse.h \
-  srcs/parse/../../includes/minishell.h \
-  srcs/parse/../../includes/../libft/libft.h \
-  srcs/parse/../../includes/../libft/get_next_line/get_next_line.h \
-  srcs/parse/../../includes/../libft/get_next_line/../libft.h \
-  srcs/parse/../../includes/../libft/ft_printf/ft_printf.h \
-  srcs/parse/../../includes/../libft/ft_printf/../libft.h
