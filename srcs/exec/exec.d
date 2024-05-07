c982140b306c76b177a8dd46442b0f4e20c6e429
Marti Vallhonrat Rafart <mvallhon@car13s2.42barcelona.com>
2024-05-07 19:00:09 +0200

    renew env for execve clean

--- srcs/exec/exec.d
+++
@@ -1,9 +0,0 @@
-srcs/exec/exec.o: srcs/exec/exec.c srcs/exec/../../includes/exec.h \
-  srcs/exec/../../includes/minishell.h \
-  srcs/exec/../../includes/../libft/libft.h \
-  srcs/exec/../../includes/../libft/get_next_line/get_next_line.h \
-  srcs/exec/../../includes/../libft/get_next_line/../libft.h \
-  srcs/exec/../../includes/../libft/ft_printf/ft_printf.h \
-  srcs/exec/../../includes/../libft/ft_printf/../libft.h \
-  srcs/exec/../../includes/parse.h srcs/exec/../../includes/error.h \
-  srcs/exec/../../includes/builtins.h
--- srcs/expander/expander_aux.d
+++
@@ -1,9 +0,0 @@
-srcs/expander/expander_aux.o: srcs/expander/expander_aux.c \
-  srcs/expander/../../includes/expander.h \
-  srcs/expander/../../includes/minishell.h \
-  srcs/expander/../../includes/../libft/libft.h \
-  srcs/expander/../../includes/../libft/get_next_line/get_next_line.h \
-  srcs/expander/../../includes/../libft/get_next_line/../libft.h \
-  srcs/expander/../../includes/../libft/ft_printf/ft_printf.h \
-  srcs/expander/../../includes/../libft/ft_printf/../libft.h \
-  srcs/expander/../../includes/parse.h
