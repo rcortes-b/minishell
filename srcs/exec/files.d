77df5c806628ed87e69391e9f41848037e40306c
Marti Vallhonrat Rafart <mvallhon@car13s2.42barcelona.com>
2024-05-08 15:56:23 +0200

    fixed hdoc clean

--- srcs/exec/files.d
+++
@@ -1,9 +0,0 @@
-srcs/exec/files.o: srcs/exec/files.c srcs/exec/../../includes/exec.h \
-  srcs/exec/../../includes/minishell.h \
-  srcs/exec/../../includes/../libft/libft.h \
-  srcs/exec/../../includes/../libft/get_next_line/get_next_line.h \
-  srcs/exec/../../includes/../libft/get_next_line/../libft.h \
-  srcs/exec/../../includes/../libft/ft_printf/ft_printf.h \
-  srcs/exec/../../includes/../libft/ft_printf/../libft.h \
-  srcs/exec/../../includes/parse.h srcs/exec/../../includes/error.h \
-  srcs/exec/../../includes/expander.h
